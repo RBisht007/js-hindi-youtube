3731e4a14b535f084524b5f8ac455b2bd648872f
RBisht007 <rakhibisht24122004@gmail.com>
2025-09-13 15:14:24 +0000

    datatypes

---
+++ .devcontainer/basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 16574124
+let accountEmail= "rakhibisht@gmail.com"
+var accountPassword="gjff4hjf"
+accountCity="kathmandu"
+
+// variable return with const keyword is not changeble
+
+console.log(accountId)
+/* 
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+// console.table is used to print the variable in ttable format
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+
---
+++ .devcontainer/basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as newer version
+//alert("hello worlld")// we are using nodee js not the browwser so alert wont work
+
+let name="rakhi"// string literal
+let age=22 // number literal
+let isApproved=true// boolean literal
+let firstName=undefined // undefined literal
+let selectedColor=null // null literal
+
+// we can use typeof operator to know the data type of varuiable
+console.log(typeof age)
+console.log(typeof name)
+
+
+
+
+
+
